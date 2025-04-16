944d13c0b1ac9fd9be66b5a223be27f5ea4e40c5
Krishna Murthy <krrish94@gmail.com>
2025-04-16 15:48:13 +0000

    Finish preprocessing script; remove pytorch3d
    Signed-off-by: Krishna Murthy <krrish94@gmail.com>

--- preprocessing/run_preprocessing.py
+++ preprocessing/run_preprocessing.py
@@ -10,7 +10,6 @@ SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 import torch
 from omegaconf import OmegaConf
-from pytorch3d.ops import knn_points
 
 from preprocessing.pointcloud_featurizer import FeatureLifter3D
 from locate3d_data.locate3d_dataset import Locate3DDataset
@@ -20,45 +19,38 @@ def main(args, start_idx, end_idx):
     cache_path = args.cache_path
 
     l3dd = Locate3DDataset(annotations_fpath = 'locate3d_data/dataset/train_scannet.json', scannet_data_dir = '/fsx-cortex/shared/datasets/scannet_ac')
-    scene_list = l3dd.list_scenes().sorted()
+    scene_list = sorted(l3dd.list_scenes())
     
-    pointcloud_featurizer_clip_cfg = OmegaConf.load("config/clip.yaml")
+    pointcloud_featurizer_clip_cfg = OmegaConf.load(os.path.join(SCRIPT_DIR, "config/clip.yaml"))
     pointcloud_featurizer_clip = FeatureLifter3D(pointcloud_featurizer_clip_cfg)
-    pointcloud_featurizer_dino_cfg = OmegaConf.load("config/dino.yaml")
+    pointcloud_featurizer_dino_cfg = OmegaConf.load(os.path.join(SCRIPT_DIR, "config/dino.yaml"))
     pointcloud_featurizer_dino = FeatureLifter3D(pointcloud_featurizer_dino_cfg)
 
     # Iterate through the dataset and cache the featurized pointclouds
     for idx in range(start_idx, end_idx):
         # Load a sample from the dataset
         scene_id = scene_list[idx][1]
-        camera_views = l3ddd.get_camera_views(*scene_list[idx])
+        camera_views = l3dd.get_camera_views(*scene_list[idx])
         
         # Early skip if the scene is already cached
         cache_file = os.path.join(cache_path, f"{scene_id}.pt")
         if os.path.exists(cache_file):
             print(f"Cache file already exists: {cache_file}")
-            print("Skipping cache creation.")
+            print(f"Skipping cache creation for scene {scene_id}")
             continue
         
+        print(f"Processing scene {scene_id} ...")
         # Build CLIP featurized pointcloud
-        clip_ptc = pointcloud_featurizer_clip.lift_frames(camera_views)
-        dino_ptc = pointcloud_featurizer_dino.lift_frames(camera_views)
-
-        # TODO: krishna for the next section
-        
-        # Aligning CLIP and DINO features
-        _knn = knn_points(obs_dino.pointcloud.points_reduced[None, ...].cuda(), obs_clip.pointcloud.points_reduced[None, ...].cuda(), K=1)
-        new_points = obs_clip.pointcloud.points_reduced[_knn.idx[0]].squeeze(1)
-        new_rgb = obs_clip.pointcloud.rgb_reduced[_knn.idx[0]].squeeze(1)
-        new_clip_features = obs_clip.pointcloud.features_reduced[_knn.idx[0]].squeeze(1)
-        new_dino_features = obs_dino.pointcloud.features_reduced
+        clip_pcd = pointcloud_featurizer_clip.lift_frames(camera_views)
+        torch.manual_seed(0) # seed the RNG so that the pointclouds are the same
+        dino_pcd = pointcloud_featurizer_dino.lift_frames(camera_views)
 
         # Creating output dictionary
         output_dict = {
-            "points": new_points,
-            "rgb": new_rgb,
-            "features_clip": new_clip_features,
-            "features_dino": new_dino_features,
+            "points": dino_pcd["points_reduced"],
+            "rgb": dino_pcd["rgb_reduced"],
+            "features_clip": clip_pcd["features_reduced"],
+            "features_dino": dino_pcd["features_reduced"],
         }
 
         # Save the output dictionary to the cache file
